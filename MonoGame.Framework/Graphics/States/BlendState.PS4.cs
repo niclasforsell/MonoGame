d94d0117a2f862ab60d7e10dcd538581b90eacd2
James Lupiani <lupiani@sickheadgames.com>
2014-03-25 11:38:26 -0500

    Added PS4 file stubs

---
+++ MonoGame.Framework/Graphics/States/BlendState.PS4.cs
@@ -0,0 +1,21 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+    public partial class BlendState
+    {
+        internal void PlatformApplyState(GraphicsDevice device)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlatformDispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
