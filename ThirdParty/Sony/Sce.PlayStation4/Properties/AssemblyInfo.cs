adee1d06f78c4c832dda1a8dafdd1f10fa4f167d
Tom Spilman <tom@sickheadgames.com>
2014-03-26 13:45:33 -0500

    Fixed assembly info.

--- ThirdParty/Sony/Sce.PlayStation4/Properties/AssemblyInfo.cs
+++ ThirdParty/Sony/Sce.PlayStation4/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonoGame.Native.PS4")]
+[assembly: AssemblyTitle("Sce.PlayStation4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonoGame.Native.PS4")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Sce.PlayStation4")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
