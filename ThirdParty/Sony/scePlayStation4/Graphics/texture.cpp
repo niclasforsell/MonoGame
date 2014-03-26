fac823a590e88bc0c609c6c85d055746022ccae2
Tom Spilman <tom@sickheadgames.com>
2014-03-25 23:54:53 -0500

    Initial test version of Texture C++ class.

---
+++ ThirdParty/Sony/scePlayStation4/Graphics/texture.cpp
@@ -0,0 +1,53 @@
+#include "Texture.h"
+
+//#include "Allocator.h"
+#include <gnm.h>
+#include <assert.h>
+
+
+Texture::Texture(sce::Gnm::Texture *texture)
+{
+	_texture = texture;
+}
+
+Texture::~Texture()
+{
+	//Allocator::Get()->release(_texture->getBaseAddress());
+	delete _texture;
+}
+
+uint32_t Texture::getWidth()
+{ 
+	return _texture->getWidth();
+}
+
+uint32_t Texture::getHeight() 
+{ 
+	return _texture->getHeight();
+}
+
+void Texture::SetData(unsigned char *data, uint32_t bytes)
+{
+	auto width = _texture->getWidth();
+	auto height = _texture->getHeight();
+	auto pixelBytes = _texture->getDataFormat().getBytesPerElement();
+
+	// We expect this call to set the entire 
+	// buffer at this point.
+	assert((width * height * pixelBytes) == bytes);
+
+	auto baseAddr = (unsigned char*)_texture->getBaseAddress();
+	auto pitch = _texture->getPitch();
+
+	if (pitch == width)
+		memcpy(baseAddr, data, bytes);
+	else
+	{
+		for (auto h=0; h < height; h++)
+		{
+			memcpy(baseAddr, data, width * pixelBytes);
+			data += width * pixelBytes;
+			baseAddr += pitch * pixelBytes;
+		}
+	}
+}
