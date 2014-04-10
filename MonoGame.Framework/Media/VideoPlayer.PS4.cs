e8704af0f5b2e909e7e9e38fd0de311e614fc5e7
Tom Spilman <tom@sickheadgames.com>
2014-04-10 16:53:28 -0500

    Added stubs for Video classes.

---
+++ MonoGame.Framework/Media/VideoPlayer.PS4.cs
@@ -0,0 +1,52 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Microsoft.Xna.Framework.Media
+{
+    public sealed partial class VideoPlayer
+    {
+        private void PlatformInitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Texture2D PlatformGetTexture()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlatformPause()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlatformResume()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlatformPlay()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlatformStop()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlatformSetVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+        private TimeSpan PlatformGetPlayPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
