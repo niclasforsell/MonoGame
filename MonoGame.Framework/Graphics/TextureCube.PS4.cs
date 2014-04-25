0c536e24d86ada9bbd4a8495893e87ad4badf152
James Lupiani <lupiani@sickheadgames.com>
2014-04-25 18:19:00 -0500

    Implemented TextureCube Get/Set

--- MonoGame.Framework/Graphics/TextureCube.PS4.cs
+++ MonoGame.Framework/Graphics/TextureCube.PS4.cs
@@ -3,6 +3,7 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
+using System.Runtime.InteropServices;
 using PS4Texture = Sce.PlayStation4.Graphics.Texture;
 using PS4TextureFormat = Sce.PlayStation4.Graphics.TextureFormat;
 
@@ -19,13 +20,32 @@ namespace Microsoft.Xna.Framework.Graphics
         private void PlatformGetData<T>(CubeMapFace cubeMapFace, T[] data)
             where T : struct
         {
-            throw new NotImplementedException();
+            var dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            var elementSizeInBytes = Marshal.SizeOf(typeof(T));
+            var pitchRow = GetPitch(this.size);
+            var length = elementSizeInBytes * pitchRow * pitchRow;
+            var faceOffset = elementSizeInBytes * (int)cubeMapFace * length;
+            var dataPtr = (IntPtr)(dataHandle.AddrOfPinnedObject().ToInt64());
+
+            unsafe
+            {
+                _texture.GetData(0, (byte*)dataPtr, (uint)faceOffset, (uint)length);
+            }
+
+            dataHandle.Free();
         }
 
-        private void PlatformSetData(CubeMapFace face, int level, IntPtr dataPtr,
-            int xOffset, int yOffset, int width, int height)
+        private void PlatformSetData<T>(CubeMapFace face, int level, IntPtr dataPtr, int xOffset, int yOffset, int width, int height)
         {
-            throw new NotImplementedException();
+            var elementSizeInBytes = Marshal.SizeOf(typeof(T));
+            var pitchRow = GetPitch(this.size);
+            var length = elementSizeInBytes * pitchRow * pitchRow;
+            var faceOffset = elementSizeInBytes * (int)face * length;
+
+            unsafe
+            {
+                _texture.SetData(0, (byte*)dataPtr, (uint)faceOffset, (uint)length);
+            }
         }
     }
 }
