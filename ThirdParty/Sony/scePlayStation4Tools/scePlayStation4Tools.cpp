6d5360bb54052563f304a199c0b015b7420ba00c
Tom Spilman <tom@sickheadgames.com>
2014-04-27 13:33:15 -0500

    Added C++ CLR project for the PS4 tool wrapper.

---
+++ ThirdParty/Sony/scePlayStation4Tools/scePlayStation4Tools.cpp
@@ -0,0 +1,14 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "scePlayStation4Tools.h"
+
+namespace sce { namespace PlayStation4 { namespace Tools {
+
+
+
+
+
+
+}}}
---
+++ ThirdParty/Sony/scePlayStation4Tools/scePlayStation4Tools.h
@@ -0,0 +1,14 @@
+// scePlayStation4Tools.h
+
+#pragma once
+
+using namespace System;
+
+namespace sce { namespace PlayStation4 { namespace Tools {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+
+}}}
