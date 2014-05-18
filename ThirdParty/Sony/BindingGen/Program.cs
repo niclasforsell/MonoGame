e3ecb47e73955aebe29a3d4603efb651f8e4329f
Tom Spilman <tom@sickheadgames.com>
2014-05-18 17:59:01 -0500

    Removed old ScePlayStation4Tools from binding generation.

--- ThirdParty/Sony/BindingGen/Program.cs
+++ ThirdParty/Sony/BindingGen/Program.cs
@@ -15,16 +15,12 @@ namespace BindingGen
         public static void Main(string[] args)
         {
             ConsoleDriver.Run(new ScePlayStation4());
-            //ConsoleDriver.Run(new ScePlayStation4Tools());
 
             // HACK: It would be nice if we could just disable the compile
             // step instead of doing hardcoded cleanup here.
             try { File.Delete("Sce.PlayStation4.dll"); } catch {}
             try { File.Delete("Sce.PlayStation4.pdb"); } catch {}
             try { File.Delete("Sce.PlayStation4.xml"); } catch {}
-            try { File.Delete("Sce.PlayStation4.Tools.Texture.dll"); } catch {}
-            try { File.Delete("Sce.PlayStation4.Tools.Texture.pdb"); } catch {}
-            try { File.Delete("Sce.PlayStation4.Tools.Texture.xml"); } catch {}
         }
     }
 }
