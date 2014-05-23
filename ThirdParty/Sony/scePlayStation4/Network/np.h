60dcefa38e71c9ed3c503f0d5e23e0532d4ed2bc
James Lupiani <lupiani@sickheadgames.com>
2014-05-23 17:37:14 -0500

    Added trophy support.

---
+++ ThirdParty/Sony/scePlayStation4/Network/np.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "predecls.h"
+#include <scebase.h>
+#include <np.h>
+
+namespace Network {
+
+enum NpResult
+{
+	Ok = SCE_OK,
+
+	ErrorInvalidArgument = SCE_NP_ERROR_INVALID_ARGUMENT,
+	ErrorUnknownPlatformType = SCE_NP_ERROR_UNKNOWN_PLATFORM_TYPE,
+	ErrorOutOfMemory = SCE_NP_ERROR_OUT_OF_MEMORY,
+	ErrorSignedOut = SCE_NP_ERROR_SIGNED_OUT,
+	ErrorUserNotFound = SCE_NP_ERROR_USER_NOT_FOUND,
+	ErrorCallbackAlreadyRegistered = SCE_NP_ERROR_CALLBACK_ALREADY_REGISTERED,
+	ErrorCallbackNotRegistered = SCE_NP_ERROR_CALLBACK_NOT_REGISTERED,
+	ErrorNotSignedUp = SCE_NP_ERROR_NOT_SIGNED_UP,
+	ErrorAgeRestriction = SCE_NP_ERROR_AGE_RESTRICTION,
+	ErrorLogout = SCE_NP_ERROR_LOGOUT,
+	ErrorLatestSystemSoftwareExist = SCE_NP_ERROR_LATEST_SYSTEM_SOFTWARE_EXIST,
+	ErrorLatestSystemSoftwareExistForTitle = SCE_NP_ERROR_LATEST_SYSTEM_SOFTWARE_EXIST_FOR_TITLE,
+	ErrorLatestPatchPkgExist = SCE_NP_ERROR_LATEST_PATCH_PKG_EXIST,
+	ErrorLatestPatchPkgDownloaded = SCE_NP_ERROR_LATEST_PATCH_PKG_DOWNLOADED,
+	ErrorInvalidSize = SCE_NP_ERROR_INVALID_SIZE,
+	ErrorAborted = SCE_NP_ERROR_ABORTED,
+	RequestMax = SCE_NP_ERROR_REQUEST_MAX,
+	RequestNotFound = SCE_NP_ERROR_REQUEST_NOT_FOUND,
+	InvalidId = SCE_NP_ERROR_INVALID_ID,
+	UtilErrorInvalidNpId = SCE_NP_UTIL_ERROR_INVALID_NP_ID,
+	UtilErrorNotMatch = SCE_NP_UTIL_ERROR_NOT_MATCH
+};
+
+class CS_API Np
+{
+	Np();
+
+public:
+	static NpResult _SetNpTitleId(const char* titleId, const uint8_t* titleSecret);
+};
+
+} // namespace Network
---
+++ ThirdParty/Sony/scePlayStation4/Network/trophySet.cpp
@@ -0,0 +1,67 @@
+#include "trophySet.h"
+#include <assert.h>
+#include <libsysmodule.h>
+
+using namespace Network;
+
+TrophySet::TrophySet()
+	: _context(SCE_NP_TROPHY_INVALID_CONTEXT)
+{
+	if (sceSysmoduleIsLoaded(SCE_SYSMODULE_NP_TROPHY) != SCE_SYSMODULE_LOADED)
+	{
+		auto ret = sceSysmoduleLoadModule(SCE_SYSMODULE_NP_TROPHY);
+		assert(ret == SCE_OK);
+	}
+}
+
+TrophySet::~TrophySet()
+{
+	if (_context != SCE_NP_TROPHY_INVALID_CONTEXT)
+	{
+		auto ret = sceNpTrophyDestroyContext(_context);
+		assert(ret == SCE_OK);
+		_context = SCE_NP_TROPHY_INVALID_CONTEXT;
+	}
+}
+
+TrophyResult TrophySet::CreateContext(const SceUserServiceUserId userId, const SceNpServiceLabel serviceLabel)
+{
+	assert(_context == SCE_NP_TROPHY_INVALID_CONTEXT);
+	SceNpTrophyHandle handle = SCE_NP_TROPHY_INVALID_HANDLE;
+
+	auto ret = sceNpTrophyCreateContext(&_context, userId, serviceLabel, 0);
+	if (ret != SCE_OK)
+		goto cleanup;
+
+	ret = sceNpTrophyCreateHandle(&handle);
+	if (handle != SCE_OK)
+		goto cleanup;
+
+	ret = sceNpTrophyRegisterContext(_context, handle, 0);
+	if (ret != SCE_OK)
+		goto cleanup;
+
+cleanup:
+	if (handle != SCE_NP_TROPHY_INVALID_HANDLE)
+		assert(sceNpTrophyDestroyHandle(handle) == SCE_OK);
+
+	return (TrophyResult)ret;
+}
+
+TrophyResult TrophySet::Unlock(int32_t trophyId, CS_OUT bool* platinumReceived)
+{
+	assert(trophyId != SCE_NP_TROPHY_INVALID_TROPHY_ID);
+	SceNpTrophyHandle handle = SCE_NP_TROPHY_INVALID_HANDLE;
+	auto ret = sceNpTrophyCreateHandle(&handle);
+	if (ret != SCE_OK)
+		return (TrophyResult)ret;
+
+	SceNpTrophyId platinumId;
+	ret = sceNpTrophyUnlockTrophy(_context, handle, trophyId, &platinumId);
+
+	*platinumReceived = platinumId != SCE_NP_TROPHY_INVALID_TROPHY_ID;
+
+	assert(sceNpTrophyDestroyHandle(handle) == SCE_OK);
+	return (TrophyResult)ret;
+}
+
