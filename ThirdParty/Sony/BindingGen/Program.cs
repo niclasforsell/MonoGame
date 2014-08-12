e4c492e6d5454fedfd058ada3ca49c5b834ad84c
Tom Spilman <tom@sickheadgames.com>
2014-08-12 17:20:12 -0500

    Updated to latest CppSharp.

--- ThirdParty/Sony/BindingGen/Program.cs
+++ ThirdParty/Sony/BindingGen/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CppSharp;
-using CppSharp.AST;
-using CppSharp.Passes;
+﻿using CppSharp;
 
 namespace BindingGen
 {
