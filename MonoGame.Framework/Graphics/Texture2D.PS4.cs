75cbf30f681eb03b2595b773aa3574dab207c55d
James Lupiani <lupiani@sickheadgames.com>
2014-10-01 18:49:05 -0500

    Add PNG support for Texture2D.FromStream. Fixes #183

--- MonoGame.Framework/Graphics/Texture2D.PS4.cs
+++ MonoGame.Framework/Graphics/Texture2D.PS4.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System.Runtime.InteropServices;
 using PS4Texture = Sce.PlayStation4.Graphics.Texture;
 using PS4TextureFormat = Sce.PlayStation4.Graphics.TextureFormat;
+using PngReader = Sce.PlayStation4.Graphics.PngReader;
+using PngReaderResult = Sce.PlayStation4.Graphics.PngReaderResult;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
@@ -66,9 +68,63 @@ namespace Microsoft.Xna.Framework.Graphics
             dataHandle.Free();
         }
 
-        private static Texture2D PlatformFromStream(GraphicsDevice graphicsDevice, Stream stream)
+        private static readonly byte[] _pngSignature = { 137, 80, 78, 71, 13, 10, 26, 10 };
+
+        private static unsafe Texture2D PlatformFromStream(GraphicsDevice graphicsDevice, Stream stream)
         {
-            throw new NotImplementedException();
+            var rawBytes = new byte[stream.Length];
+            stream.Read(rawBytes, 0, (int)stream.Length);
+
+            if (rawBytes.Length >= _pngSignature.Length)
+            {
+                var isPng = true;
+                for (var i = 0; i < _pngSignature.Length; i++)
+                {
+                    if (rawBytes[i] != _pngSignature[i])
+                    {
+                        isPng = false;
+                        break;
+                    }
+                }
+
+                if (isPng)
+                    return FromPngStream(graphicsDevice, rawBytes);
+            }
+
+            throw new NotSupportedException("File corrupt or format not supported.");
+        }
+
+        private static unsafe Texture2D FromPngStream(GraphicsDevice graphicsDevice, byte[] rawBytes)
+        {
+            using (var reader = new PngReader())
+            {
+                PngReaderResult result = PngReaderResult.Fatal;
+
+                result = reader.Initialize(8192); // TODO: Is there a good place to get this size hint?
+                if (result != PngReaderResult.OK)
+                    throw new Exception("Error initializing PNG reader: " + result.ToString());
+
+                fixed (byte* compressed = rawBytes)
+                    result = reader.LoadCompressed(compressed, rawBytes.Length);
+
+                if (result != PngReaderResult.OK)
+                    throw new Exception("Unable to load PNG from stream: " + result.ToString());
+
+                var decomp = new byte[reader.OutputSize];
+                fixed (byte* uncompressed = decomp)
+                {
+                    result = reader.OutputUncompressed(uncompressed, decomp.Length);
+                    if (result != PngReaderResult.OK)
+                        throw new Exception("Error decompressing PNG: " + result.ToString());
+                }
+
+                var texture = new Texture2D(graphicsDevice, reader.Width, reader.Height);
+                texture.SetData(decomp);
+
+                reader.Cleanup();
+
+                return texture;
+            }
         }
 
         private void PlatformSaveAsJpeg(Stream stream, int width, int height)
